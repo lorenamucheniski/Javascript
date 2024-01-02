174fd41c7c003d2cd1f5f01c44ee96318743d716
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-02 15:51:54 -0300

    feat: declaring variables

---
+++ aula3-variaveis.js
@@ -0,0 +1,12 @@
+//definindo um valor para a variável
+let nome = 'Samuel'
+
+console.log(nome, 'nasceu em 08 de Abril de 2020.')
+console.log('Em 2021', nome, 'foi para a escola.')
+console.log(nome, 'ama os animais e também brincar em contato com a natureza.')
+
+//declarando a variável sem inicializa-la
+let nome1
+nome1 = 'Lorena'
+console.log(nome1)
+
