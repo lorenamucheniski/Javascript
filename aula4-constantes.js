fdf8673d1a82dcf5804e51d13c594671858cfb80
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-03 21:13:09 -0300

    feat: ex imc

---
+++ aula4-constantes.js
@@ -0,0 +1,12 @@
+/**
+ * uma constante sempre precisa ter seu valor definido na sua criação;
+ * Para declarar uma constatnte use const;
+ **/ 
+
+const nome = 'Luiz Otávio Miranda'
+const idade= 30
+const peso = 84
+const altura = 1.8
+let imc = peso/(altura*altura)
+
+console.log(nome,'tem', idade, 'anos, pesa', peso,'kg, tem',altura,'de altura e seu imc é de',imc,'.')
