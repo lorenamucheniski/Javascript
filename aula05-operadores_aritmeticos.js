8bbc378b3215e5886f322b86279960d45e3bc1fd
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-08 09:16:34 -0300

    feat: arithmetic operators

---
+++ aula05-operadores_aritmeticos.js
@@ -0,0 +1,13 @@
+/** Operadores aritméticos:
+ * + adição / - subtração / / divisão / * multiplicação
+ *  ** potenciação
+ *  % resto da divisão
+ */
+
+/**
+ * Ordem de precendencia
+ * () sempre o que estiver em parentes será execultado primeiro
+ *  ** potenciação
+ *  * / % 
+ *  + -
+*/
