29777d6beca50726a11b2b3756faf303ae17093d
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-08 09:57:07 -0300

    feat: exercise

---
+++ aula6/exercicios01.js
@@ -0,0 +1,8 @@
+let num1 = prompt("Digite um número:")
+let num2 = prompt('Digite outro número:')
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2
+alert("o resultado da soma é de: " + resultado)
