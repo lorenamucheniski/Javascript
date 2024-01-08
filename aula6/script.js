d42aa5549eaf7ae192e3421757b60cb4d847f635
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-08 09:56:24 -0300

    feat: alert/confirm/prompt

---
+++ aula6/script.js
@@ -0,0 +1,6 @@
+alert('Bom dia!') //Faz um alerta na página
+
+confirm ('Deseja realmente apagar os dados?') /**exibe uma mensagem para o usuário confirmar ou não, retornando
+os valores true(para sim) or false(para não)*/
+
+prompt('Digite seu nome:') // exibe uma caixinha para o usuário digitar alguma coisa
