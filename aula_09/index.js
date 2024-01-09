3b602885b38dfbe0001eff7ff18f70d766622081
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-09 16:55:33 -0300

    feat: numbers

---
+++ aula_09/index.js
@@ -0,0 +1,12 @@
+let num1 = 55.51651616
+let num2 = 10
+
+console.log(num1.toString() + num2)  // converte o número temporariamente para string
+console.log(num1.toString(2))  // representação binária de um número
+console.log(num1.toFixed(2))  // definir a quantidade de casas decimais após a vírgula
+console.log(Number.isInteger(num2))  // para saber se o número é inteiro
+
+
+
+
+
