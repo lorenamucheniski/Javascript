01796071397bdb37e2ec159d06155f1fb9f5c8de
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-10 13:16:43 -0300

    feat: exercise with html / css / javascript

--- Exercicios/Exercicio1/script.js
+++ Exercicios/Exercicio1/script.js
@@ -1,8 +1,8 @@
 const numero= Number(prompt ('Digite um número'));
 const numeroTitulo = document.getElementById('numero-titulo'); //pegando o elemento do html cujo ID é 'número-titulo'
 numeroTitulo.innerHTML = numero;  //constante numeroTitulo recebe o valor de numero e passa para o HTML
-
 const texto = document.getElementById('texto');
+
 texto.innerHTML += `<p>Seu número + 2 é ${numero + 2}</p>`;
 texto.innerHTML += `<p>A raiz quadrada de ${numero} é igual a ${numero ** (1/2)}`;
 texto.innerHTML += `<p>O numero ${numero} é inteiro? ${Number.isInteger(numero)}`;
