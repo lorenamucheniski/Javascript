37a431bc0a4328260e5240da7f157f72056a5415
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-11 14:29:18 -0300

    feat: arrays

--- aula_10/arrays.js
+++ aula_10/arrays.js
@@ -25,6 +25,19 @@ alunos.unshift('Ducinei')
 console.log(alunos)
 
 console.log('Removendo um item do final: ')
-const removido = alunos.pop()
+const removido = alunos.pop()    //remove do final da array
 console.log(removido)
 console.log(alunos)
+
+console.log('Removendo um item do inicio: ')
+const removido2 = alunos.shift()   //remove do inicio da array
+console.log(removido2)
+console.log(alunos)
+
+console.log('Fatiamento de array: ')   //fatiando uma array
+console.log(alunos.slice(0,5))         //lembrando que o ultimo indice fica de fora
+
+console.log('Fatiamento de array usando negativos: ')   //fatiando uma array
+console.log(alunos.slice(0,-2))                         //retira os dois ultimo itens da array
+
+console.log(alunos instanceof Array)    //se estiver com duvida se esta trabalhando com array
