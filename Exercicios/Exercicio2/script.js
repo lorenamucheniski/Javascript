fdb8b9daae2694db2e61355619cff921d3b5a4bf
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-14 20:35:03 -0300

    feat: finalizando formulário de cadastro

--- Exercicios/Exercicio2/script.js
+++ Exercicios/Exercicio2/script.js
@@ -1,14 +1,29 @@
-function meuEscopo () {
-    const form = document.querySelector('.form');
-    const resultado = document.querySelector('.resultado')
+function meuEscopo() {
+    const form = document.querySelector('.form');               //selecionando o formulário através da classe
+    const resultado = document.querySelector('.resultado');      //selecionando a div para exibir o resultado da nossa funçao
 
-    function recebeEventoForm (evento) {
+    const pessoas = [];       //criando uma array para armazenar os valores
+
+    //criando uma função recebeEventoForm para receber o formulário
+    function recebeEventoForm(evento) {
         evento.preventDefault();
 
+        //selecionando os itens da minha form
         const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
         const nasc = form.querySelector('.nasc');
-        const cpf = form.querySelector('cpf');
-    };
 
+        //adicionando os valores na array pessoas
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            nasc: nasc.value,
+        });
+
+        resultado.innerHTML += `<p>Dados inseridos: ${nome.value} ${sobrenome.value} Nascimento: ${nasc.value}</p>`;
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
 }
+
+meuEscopo();
