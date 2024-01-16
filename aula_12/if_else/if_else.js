483fc2669ce9d9c941b01f0d821665430de4129a
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-16 06:59:11 -0300

    feat: if_else

---
+++ aula_12/if_else/if_else.js
@@ -0,0 +1,9 @@
+const hora = 5;
+
+if (hora >= 0 && hora <=11) {
+    console.log('Bom dia')
+} else if (hora > 11 && hora <= 18){
+    console.log('Boa tarde')
+} else  {
+    console.log('Boa noite')
+}
