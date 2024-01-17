cd649f14845829b74a5e9157cb65b5483b2770ad
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-17 14:21:56 -0300

    feat: adicionando comentários ao código

--- Exercicios/Exercicio3/script.js
+++ Exercicios/Exercicio3/script.js
@@ -2,19 +2,19 @@
 //selecionando o formulário e guardando em uma constante 'form'
 const form = document.querySelector('.form');
 
-//adicionando um evento de submit e criando uma função
+//adicionando um evento de submit e criando uma função e= evento
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    setResultado('Olá mundo')
+    e.preventDefault();                         
+    setResultado('Olá mundo')                        //retornando o valor de resultado e exibindo no html
 });
 
 function setResultado (msg) {
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = '';
 
-    //criando uma const e inserinod um paragrafo na div resultado
-    const p = document.createElement('p');
-    p.classList.add('paragrafo-resultado');
-    p.innerHTML = 'Alguma coisa';
-    resultado.appendChild(p);
+    //criando uma const e inserindo um paragrafo na div resultado
+    const p = document.createElement('p');           //criando elemento p
+    p.classList.add('paragrafo-resultado');          //adicioandno uma classe no meu elemento p
+    p.innerHTML = 'Alguma coisa';                    //exibir no meu documento html
+    resultado.appendChild(p);                        //tornando a const p filha da const resultado
 }
