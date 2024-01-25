5f526f0057b0d8bb7d668895007cf70cd18e380d
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-25 11:45:40 -0300

    feat: 25-01 Exercício com NodeList

---
+++ Exercicios/Exercicios4/Node_list/script.js
@@ -0,0 +1,13 @@
+
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+//pegando a coe de fundo de body e passando para os parágrafos
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+for (let p of ps) {
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#FFFFFF'
+}
