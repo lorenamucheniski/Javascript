5386258686db2e75862cf6cee96e8e314bcf08ee
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-24 22:08:17 -0300

    feat: for of

---
+++ aula_12/for_of/script.js
@@ -0,0 +1,9 @@
+const nome = 'Lorena Mucheniski'
+
+for (let valor of nome) {
+    console.log(valor);
+}
+
+// For clássico - usado geralmente com iteráveis (array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (iteráveis, arrays ou strings)
