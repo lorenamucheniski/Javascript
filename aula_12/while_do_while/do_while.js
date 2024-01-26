ca7062f496c9cbd12c07ba70e85f2400b1dd9e2d
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-26 15:38:27 -0300

    feat: 26-01 do_while

---
+++ aula_12/while_do_while/do_while.js
@@ -0,0 +1,12 @@
+function sorteio (min, max) {
+    const s = Math.sorteio() * (max - min) + min;
+    return Math.floor(s);
+}
+
+const min = 1;
+const max = 50;
+let sort = 10;
+
+do {
+    console.log(sort);
+} while (sort !== 10);
