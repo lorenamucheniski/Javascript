39ed67483a544d9c3fbd19ab3ae379c7ad995c5d
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-26 16:03:35 -0300

    feat: 26-01 Exercício de lógica 1

---
+++ Exercicios/Exercicios5/lógica1/script.js
@@ -0,0 +1,6 @@
+//RETORNANDO MAIOR VALOR
+
+//criando uma função dentro da constante max 
+                  //x maior que y? se sim retorne x se não (:) retorne y  
+const max = (x, y) => x > y ? x : y;
+console.log(max(112,58));
