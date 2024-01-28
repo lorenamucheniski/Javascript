d22afdc1f0170dbbb1f9c0d1d3f8d805768581e6
lorenamucheniski <lomucheniski25@gmail.com>
2024-01-27 23:07:53 -0300

    feat: 27-01 exercicío temporizador

---
+++ Exercicios/Exercicios6/Timer/script.js
@@ -0,0 +1,39 @@
+//fução para retornar os segundos e a hora zerada
+function criaHoraDosSegundos(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+};
+
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+let segundos = 0;
+let timer;
+
+function iniciaRelogio(){
+    timer = setInterval(function(){
+        segundos ++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function(event){
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+
+pausar.addEventListener('click', function(event){
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+});
+
+zerar.addEventListener('click', function(event){
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
---
+++ aula13/time/script.js
@@ -0,0 +1,20 @@
+//SETINTERVAL E SETTIMEOUT
+function mostrarHora(){
+    let data = new Date();
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false 
+    });
+}
+
+const timer = setInterval(function() {
+    console.log(mostrarHora());
+}, 1000)
+
+setTimeout(function() {
+    clearInterval(timer);
+}, 3000)
+
+setTimeout(function() {
+    console.log('Olá Mundo!')
+}, 5000)
